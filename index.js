e388b2c7778aa4397df00de111a29bee6cfb1d99
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-08-18 13:19:51 +0530

    done with some main frame

--- index.js
+++ index.js
@@ -12,5 +12,24 @@ while (perm.length < 255) {
 
 var lerp = (a, b, t) => a + (b - a) * t;
 var noise = x => {
+    x = x % 255;
     return lerp(perm[Math.floor(x), Math.ceil(x)], x - Math.floor(x));
+}
+
+function loop() {
+    ctx.fillStyle = "#19f";
+    ctx.fillRect(0, 0, c.width, c.height);
+
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.moveTo(0, c.height);
+
+    for (let i = 0; i < c.width; i++)
+        ctx.lineTo(i, noise(i));
+
+    ctx.lineTo(c.width, c.height);
+    ctx.fill()
+    requestAnimationFrame(loop);
+
+
 }
