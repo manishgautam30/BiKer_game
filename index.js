85004d9bf50be9d56d58746427f5499e16faf4e2
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-08-18 12:56:34 +0530

    a character perm

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ var c = document.createElement("canvas");
 var ctx = c.getContext("2d");
 c.width = 500;
 c.height = 300;
+document.body.appendChild(c);
+
+var perm = [];
+while (perm.length < 255) {
+    while (perm.includes(val = Math.floor(Math.random() * 255)));
+    perm.push(val);
+}
